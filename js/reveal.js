e670f5874b7ea69becf6cc5ace92ddce1612c53f
juanjo1998 <54190509+juanjo1998@users.noreply.github.com>
2021-11-03 18:45:40 -0500

    First Commit

---
+++ js/reveal.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+sr.reveal("#presentation",{
+    duration: 2000,
+    origin:"top",
+    distance:"50px"
+})
+
+sr.reveal("#inside",{
+    duration: 4000,
+    origin:"top",
+    distance:"50px"
+})
+
+sr.reveal("#outside",{
+    duration: 4000,
+    origin:"top",
+    distance:"50px"
+})
+
+sr.reveal("#pricing",{
+    duration: 2000,
+    origin:"top",
+    distance:"50px"
+})
+
+
